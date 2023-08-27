323ead11a0c198e75c45735b5504fec0776fc077
lucasil <lucas07.macedo@gmail.com>
2023-08-27 18:56:55 -0300

    atualização de código e formatação

--- form_imc.php
+++ form_imc.php
@@ -1,50 +1,55 @@
-<?php
-
-if ($_POST) {
-    $peso = $_POST['peso'];
-    $altura = $_POST['altura'];
-    
-    $imc = $peso / ($altura*$altura);
-    
-    echo "Seu IMC: " . $imc . "<br>";
-
-    if ($imc < 17 ){
-        echo "Muito abaixo do peso"; 
-    } else if($imc >= 17 && $imc < 18.5) {
-        echo "Abaixo do peso";
-    } else if ($imc >= 18.5 && $imc < 25) {
-        echo "Peso normal";
-    } else if ($imc >= 25 && $imc < 30) {
-        echo "Acima do peso";
-    } else if($imc >= 30 && $imc < 35) {
-        echo "Obesidade I";
-    } else if($imc >= 35 && $imc < 40) {
-        echo "Obesidade II (severa)";
-    } else {
-        echo "Obesidade III (mórbida";
-    }	
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Calculadora de IMC</title>
 </head>
 <body>
-    <form method="post">
-        <label for="peso">Peso</label>
-        <input type="text" name="peso" id="peso" required>
-        <br>
-        <label for="altura">Altura</label>
-        <input type="text" name="altura" id="altura" required>
-        <br>
+    <div id="main">
+        <h1>Atividade Cálculo IMC e Cálculo de Média</h1>
+        <h2>Cálculo do IMC</h2>
+        <form method="post">
+            <label for="peso">Peso (utilize "." ao inserir as medidas)</label><br>
+            <input type="text" name="peso" id="peso" required>
+            
+            <br>
+            
+            <label for="altura">Altura (utilize "." ao inserir as medidas)</label><br>
+            <input type="text" name="altura" id="altura" required>
+            
+            <br>
 
-        <button type="submit">Calcular IMC</button>
-    </form>
+            <button type="submit">Calcular IMC</button>
+        </form>
 
+        <?php
+        if ($_POST) {
+            $peso = $_POST['peso'];
+            $altura = $_POST['altura'];
+            
+            $imc = $peso / ($altura*$altura);
+            
+            echo "<p>Seu IMC: " . $imc . "</p><br>";
+
+            if ($imc < 17 ){
+                echo "<p>Muito abaixo do peso</p>"; 
+            } else if($imc >= 17 && $imc < 18.5) {
+                echo "<p>Abaixo do peso</p>";
+            } else if ($imc >= 18.5 && $imc < 25) {
+                echo "<p>Peso normal</p>";
+            } else if ($imc >= 25 && $imc < 30) {
+                echo "<p>Acima do peso</p>";
+            } else if($imc >= 30 && $imc < 35) {
+                echo "<p>Obesidade I</p>";
+            } else if($imc >= 35 && $imc < 40) {
+                echo "<p>Obesidade II (severa)</p>";
+            } else {
+                echo "<p>Obesidade III (mórbida)</p>";
+            }	
+        }
+        ?>
+    </div>
 </body>
 </html>
--- form_notas.php
+++ form_notas.php
@@ -1,41 +1,46 @@
-<?php
-
-if ($_POST) {
-    $nota_1 = $_POST['nota_1'];
-    $nota_2 = $_POST['nota_2'];
-    
-    $media = ($nota_1 + $nota_2)/2;
-    
-    echo "Média das notas: " . $media . "<br>";
-    if($media >= 6) {
-        echo "Aprovado";
-    } else if($media < 6 && $media >= 4) {
-        echo "Exame final";
-    } else {
-        echo "Reprovado";
-    }
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Média das notas</title>
 </head>
 <body>
-    
-   
-    <form action="form_notas.php" method="post">
-        <label for="nota_1">Nota 1</label>
-        <input type="text" name="nota_1" id="nota_1" required>
-        <br>
-        <label for="nota_2">Nota 2</label>
-        <input type="text" name="nota_2" id="nota_2" required>
-        <br>
-        <button type="submit">Calcular Média</button>
-    </form>
+    <div id="main">
+        <h1>Atividade Cálculo IMC e Cálculo de Média</h1>
+        <h2>Cálculo de Média e situação do aluno</h2>
+        <form action="form_notas.php" method="post">
+            <label for="nota_1">Nota 1 (utilize "." ao inserir as medidas)</label><br>
+            <input type="text" name="nota_1" id="nota_1" required>
+            
+            <br>
+            
+            <label for="nota_2">Nota 2 (utilize "." ao inserir as medidas)</label><br>
+            <input type="text" name="nota_2" id="nota_2" required>
+            
+            <br>
+            
+            <button type="submit">Calcular Média</button>
+        </form>
+
+        <?php
+        if ($_POST) {
+            $nota_1 = $_POST['nota_1'];
+            $nota_2 = $_POST['nota_2'];
+            
+            $media = ($nota_1 + $nota_2)/2;
+            
+            echo "Média das notas: " . $media . "<br>";
+            if($media >= 6) {
+                echo "Aprovado";
+            } else if($media < 6 && $media >= 4) {
+                echo "Exame final";
+            } else {
+                echo "Reprovado";
+            }
+        }
+        ?>
+    </div>
 </body>
 </html>
